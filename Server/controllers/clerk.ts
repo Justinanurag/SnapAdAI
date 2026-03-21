a89f0d43d42249360b417467aa1702c428f04ec8
justinanurag <justinanurag0.2@gmail.com>
2026-03-21 23:12:10 +0530

    server set up and database connection proper with prisam orm and neon DB and integrate middleware and auth from clerk schema design for users model as well as project model

---
+++ Server/controllers/clerk.ts
@@ -0,0 +1,99 @@
+import { Request, Response } from "express";
+import { verifyWebhook } from "@clerk/express/webhooks";
+import { prisma } from "../configs/prisma.js";
+const clerkWebhook = async (req: Request, res: Response) => {
+  try {
+    const evt = await verifyWebhook(req);
+    //Getting data from the request
+
+    const { data, type } = evt;
+    //switch case for different events
+    switch (type) {
+      case "user.created": {
+        await prisma.user.create({
+          data: {
+            id: data.id,
+            email: data.email_addresses[0]?.email_address,
+            name: data?.first_name + " " + data?.last_name,
+            image: data?.image_url,
+          },
+        });
+        break;
+      }
+
+      case "user.updated": {
+        await prisma.user.update({
+          where: {
+            id: data.id,
+          },
+          data: {
+            email: data.email_addresses[0]?.email_address,
+            name: data?.first_name + " " + data?.last_name,
+            image: data?.image_url,
+          },
+        });
+        break;
+      }
+      case "user.deleted": {
+        await prisma.user.delete({
+          where: {
+            id: data.id,
+          },
+        });
+        break;
+      }
+      case "paymentAttempt.updated": {
+        if (
+          (data.charge_type === "recurring" ||
+            data.charge_type === "checkout") &&
+          data.status === "paid"
+        ) {
+          const credits = { pro: 80, premimum: 240 };
+          const clerkUserId = data?.payer?.user_id;
+          const planId: keyof typeof credits =
+            data?.subscription_items[0]?.plan?.slug;
+
+          if (planId !== "pro" && planId !== "premimum") {
+            return res.status(400).json({
+              message: "Invalid plan",
+            });
+          }
+          console.log(planId);
+          try {
+            await prisma.user.update({
+              where: { id: clerkUserId },
+              data: {
+                credits: {
+                  increment: credits[planId],
+                },
+              },
+            });
+            console.log(`✅ Credits added for ${planId}`);
+          } catch (error) {
+            console.error("❌ User update failed:", error);
+            return res.status(400).json({
+              message: "DB update failed",
+            });
+          }
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+    res
+      .status(200)
+      .json({
+        received: true,
+        message: "Webhook received successfully: " + type,
+      });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(400)
+      .json({ received: false, message: "Webhook received failed" });
+  }
+};
+
+export default clerkWebhook;
---
+++ Server/server.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./configs/prisma.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhook from "./controllers/clerk.js";
+dotenv.config();
+
+const app = express();
+app.post("/api/clerk", express.raw({ type: "application/json" }), clerkWebhook);
+app.use(express.json());
+app.use(clerkMiddleware());
+
+const PORT = process.env.PORT || 5000;
+connectDB(); //connection check
+//middleware
+app.use(cors());
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from the server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
