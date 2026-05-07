07e5391d2563dc9ead2ee0f75c52eb73526f5c28
justinanurag <justinanurag0.2@gmail.com>
2026-05-07 09:33:02 +0530

    change file save locally to cloudinaly

--- Server/configs/multer.ts
+++ Server/configs/multer.ts
@@ -1,13 +1,9 @@
 import multer from "multer";
 
-const storage=multer.diskStorage({
-    destination:"./uploads",
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
-})
+const storage = multer.memoryStorage();
 
-const upload=multer({storage});
-
+const upload = multer({
+  storage,
+});
 
 export default upload;
